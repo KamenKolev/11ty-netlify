3a404eb1a574311f3dfd768d8253113b5ea5fc22
K_Kolev <kkolev@protonmail.com>
2019-07-10 11:25:36 +0300

    added admin folder to passthrough

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("img");
   eleventyConfig.addPassthroughCopy("css");
+  eleventyConfig.addPassthroughCopy("admin");
 
   eleventyConfig.addCollection("postsReversed", collection =>
     collection.getFilteredByTag("post").reverse(),
