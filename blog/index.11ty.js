4a89a7287e2cf5edaef32670426336056f1308c1
K_Kolev <kkolev@protonmail.com>
2019-07-10 10:54:05 +0300

    adding sorting to blog posts ( the manual way? )

--- blog/index.11ty.js
+++ blog/index.11ty.js
@@ -10,7 +10,13 @@ module.exports = class Blog {
     <div>
       <ul>
         ${collections.post
-          .map(post => `<li><a href="${post.url}">${post.data.title}</a></li>`)
+          .map(
+            post =>
+              `<li>
+                <a href="${post.url}">${post.data.title}</a>
+              </li>`
+          )
+          .reverse()
           .join("")}
       </ul>
     </div>
